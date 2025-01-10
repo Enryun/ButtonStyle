35128ebce6919152deb570a74bc18b6f4fce50db
JamesThang <jamesthang1996@gmail.com>
2025-01-10 15:55:47 +0700

    configure package

--- Package.swift
+++ Package.swift
@@ -5,17 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "ButtonStyle",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ButtonStyle",
-            targets: ["ButtonStyle"]),
+            targets: ["ButtonStyle"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ButtonStyle"),
-
+            name: "ButtonStyle",
+            path: "./Sources/ButtonStyle"
+        ),
     ]
 )
