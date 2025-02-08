bc22aaf766d820a988c5dbd5defd642aaf97a8b8
JamesThang <jamesthang1996@gmail.com>
2025-02-08 23:57:59 +0700

    add extension for capsule button style

--- Sources/ButtonStyle/CapsuleButtonStyle.swift
+++ Sources/ButtonStyle/CapsuleButtonStyle.swift
@@ -61,3 +61,58 @@ public struct CapsuleButtonStyle<TextShapeStyle: ShapeStyle, BackgroundShapeStyl
             }
     }
 }
+
+public extension ButtonStyle where Self == CapsuleButtonStyle<Color, Color> {
+    /**
+     Creates a capsule-shaped button style with solid colors for both the text and background.
+     
+     - Parameters:
+     - textColor: The color used for the button text. Defaults to `.white`.
+     - backgroundColor: The color used for the button background. Defaults to `.blue`.
+     - verticalPadding: The amount of vertical padding inside the button. Defaults to `10`.
+     - horizontalPadding: The amount of horizontal padding inside the button. Defaults to `20`.
+     
+     - Returns: A `CapsuleButtonStyle` configured with the specified text color, background color, and padding.
+     */
+    static func capsule(
+        textColor: Color = .white,
+        backgroundColor: Color = .blue,
+        verticalPadding: CGFloat = 10,
+        horizontalPadding: CGFloat = 20
+    ) -> Self {
+        .init(
+            textColor: textColor,
+            backgroundColor: backgroundColor,
+            verticalPadding: verticalPadding,
+            horizontalPadding: horizontalPadding
+        )
+    }
+}
+
+public extension ButtonStyle where Self == CapsuleButtonStyle<Color, LinearGradient> {
+    /**
+     Creates a capsule-shaped button style with a solid color for the text and a `LinearGradient` for the background.
+     
+     - Parameters:
+     - textColor: The color used for the button text. Defaults to `.white`.
+     - backgroundColor: The gradient used for the button background.
+     - verticalPadding: The amount of vertical padding inside the button. Defaults to `10`.
+     - horizontalPadding: The amount of horizontal padding inside the button. Defaults to `20`.
+     
+     - Returns: A `CapsuleButtonStyle` configured with the specified text color, gradient background, and padding.
+     */
+    static func capsule(
+        textColor: Color = .white,
+        backgroundColor: LinearGradient,
+        verticalPadding: CGFloat = 10,
+        horizontalPadding: CGFloat = 20
+    ) -> Self {
+        .init(
+            textColor: textColor,
+            backgroundColor: backgroundColor,
+            verticalPadding: verticalPadding,
+            horizontalPadding: horizontalPadding
+        )
+    }
+}
+
